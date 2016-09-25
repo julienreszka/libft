0302441f4dda9a85ba935b0ac415ec935d369e22
Julien Reszka <contact.reszkajulien@gmail.com>
2016-09-25 14:00:32 +0200

    ajout de la todolist et du fichier auteur

---
+++ ft_memset.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jreszka <jreszka@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 12:57:21 by jreszka           #+#    #+#             */
+/*   Updated: 2016/09/25 13:59:22 by jreszka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	The memset() function fills the first n bytes of memory area pointed 
+**	to by s with constant byte c.
+**
+**	The memset() function returns a pointer to the memory area s.
+*/
+
